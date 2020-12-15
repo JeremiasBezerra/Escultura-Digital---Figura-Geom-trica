a05b3c18c8469b914225e38610e43d0b5b9e3357
Jeremias Bezerra Sobrinho <jeremias.bs@gmail.com>
2020-12-15 01:05:03 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+## Projeto de Programação Avançada - Segunda Unidade - Parte II
+
+### Componentes:
+
+**_Jeremias Bezerra Sobrinho - Matrícula: 20180030774_**
+
+**_Kaline Souza dos Santos - Matrícula: 20160147330_**
+<hr>
---
+++ sculptor.h
@@ -0,0 +1,34 @@
+#ifndef SCULPTOR_H
+#define SCULPTOR_H
+
+/** @brief Estrutura Voxel que define como serão os pontos da escultura.
+* r, g e b são variáveis de cor;
+* a determina transparência;
+* isOn determina se o bloco está ou não na escultura.
+*/
+
+struct Voxel {
+  float r,g,b;
+  float a;
+  bool isOn;
+};
+
+/** @brief Classe Sculptor usada para definir as propriedades da escultura.
+*/
+
+class Sculptor {
+protected:
+  Voxel ***v;
+  int nx,ny,nz;
+  float r,g,b,a;
+  
+public:
+  Sculptor(int _nx, int _ny, int _nz);
+  ~Sculptor();
+  void setColor(float r, float g, float b, float alpha);
+  void putVoxel(int x, int y, int z);
+  void cutVoxel(int x, int y, int z);
+  void writeOFF(char* filename);
+};
+
+#endif // SCULPTOR_H
